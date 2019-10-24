73164726116c516ff7796c4d3f5d1b8c321ce7b6
Andrew Kinstler <andrewckinstler@gmail.com>
2019-10-24 17:57:30 -0600

    Remove unnecessary tests from Hydration class

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -17,21 +17,8 @@ describe('Hydration', () => {
   it('should be a function', function() {
     expect(Hydration).to.be.a('function');
   });
-  
-  it('should calculate average fluid ozs consumed per day for all time', function() {
-    expect(hydration.returnUserAvgAllTime(1, 'numOunces')).to.equal(59);
-  });
-
-  it('should calculate fluid ounces by specific date', function() {
-    expect(hydration.returnDataByDate(1, '2019/06/15', 'numOunces')).to.equal(37);
-  });
-
-  it('should calculate water consumption over a week period', function() {
-    expect(hydration.returnUserDataByWeek(3, '2019/06/21', 'numOunces')).to.deep.eql([47, 99, 28, 40, 85, 51, 41]);
-  });
 
   it('should return a boolean determining whether the user drank enough water for the last week', () => {
     expect(hydration.returnDidUserDrinkEnoughWater(1, '2019/06/21', 'numOunces')).to.equal(true);
-  })
-  
+  });  
 });
